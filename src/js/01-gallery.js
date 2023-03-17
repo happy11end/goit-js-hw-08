1322cad47b41742bce26dac645bff69e20e5635a
happy11end <romansenushun14@gmail.com>
2023-03-17 17:53:43 +0200

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const gallery = document.getElementsByClassName("gallery")[0]
+
+
+createImages(galleryItems)
+
+
+function createImages(e) {
+   const images = e.reduce ((arr,{ preview, original, description }) => {
+       return arr + `<a class="gallery__item" href="${original}"><img class="gallery__image"
+      src="${preview}" alt="${description}"/></a> ` 
+   },"")
+    gallery.insertAdjacentHTML('beforeend', images)    
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionsData: "alt",
+    captionDelay:250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+const LocalStorage_Key = 'feedback-form-state';
+
+form.addEventListener('input',
+  throttle(e => {   
+    const data = { email: email.value, message: message.value };
+    localStorage.setItem(LocalStorage_Key, JSON.stringify(data))
+  }, 500)  
+);
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    form.reset();
+    localStorage.removeItem(LocalStorage_Key);
+});
+
+function load(key) {
+  try {
+    const readData = localStorage.getItem(key);
+    return readData === null ? undefined : JSON.parse(readData);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const dataSave = load(LocalStorage_Key);
+if (dataSave) {
+  email.value = dataSave.email;
+  message.value = dataSave.message
+}
